7134b87c6c3635e99fcec792b939a0ae075f7e42
Мельников Андрей Александрович <melnikov.aaleksan@devzone.local>
2024-10-23 11:53:38 +0500

    chore: move app files to src directory

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
